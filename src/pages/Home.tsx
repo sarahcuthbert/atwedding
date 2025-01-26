11b1d57a2ff328b70a0c4f7e38507333311eb361
sarahcuthbert <sarahrcuthbert@gmail.com>
2025-01-26 22:01:38 +0000

    Add Home text

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,17 @@
+import {Stack, Typography} from "@mui/material";
 
 export const Home = () => {
     return (
-        <>
-            <h1>Home</h1>
-        </>
+        <Stack spacing={2} useFlexGap >
+            <Typography variant="h3" mb="2rem">You are cordially invited to the wedding of</Typography>
+            <Typography variant="h2" sx={{fontStyle: "normal"}}>Amy Cuthbert</Typography>
+            <Typography variant="h4" >&</Typography>
+            <Typography variant="h2" sx={{fontStyle: "normal"}}>Tristan Hopper</Typography>
+            <Typography variant="h5" >on</Typography>
+            <Typography variant="h4" sx={{fontStyle: "normal"}}>October 18, 2025</Typography>
+            <Typography variant="h5" >at</Typography>
+            <Typography variant="h4" sx={{fontStyle: "normal"}}>Farbridge, Chichester</Typography>
+            <Typography variant="h4" sx={{fontStyle: "normal"}}>UK</Typography>
+        </Stack>
     )
 }
