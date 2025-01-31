babc67e15294ea4224b52a7fc88c69e2a2589ad8
sarahcuthbert <sarahrcuthbert@gmail.com>
2025-01-31 11:31:18 +0000

    Use smaller googleapi package to make it smaller to deploy and secret values instead of config json

--- server/helpers/googleConfig.ts
+++ server/helpers/googleConfig.ts
@@ -1,12 +1,16 @@
-import {google} from "googleapis";
+import {GoogleAuth} from "google-auth-library";
+import {sheets} from '@googleapis/sheets'
 
 export const configureSheets = () => {
     const jsonKeyFile = process.env.GOOGLE_SERVICE_ACCOUNT_KEY_FILE;
     if(!jsonKeyFile) throw Error("Missing Service Account Key");
-    const jwtClient = new google.auth.GoogleAuth({
-        keyFile: jsonKeyFile,
+    const jwtClient = new GoogleAuth({
+        credentials: {
+            "client_email": process.env.GOOGLE_CLIENT_ID,
+            "private_key": process.env.GOOGLE_CLIENT_KEY,
+        },
         scopes: ['https://www.googleapis.com/auth/spreadsheets'],
     });
 
-    return google.sheets({version: 'v4', auth: jwtClient});
+    return sheets({version: 'v4', auth: jwtClient});
 }
