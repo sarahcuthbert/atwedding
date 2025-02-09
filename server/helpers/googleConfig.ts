9a9b74b2e8dd7f6fa2d24c65d2a5f58d240674d9
Sarah Personal <sarahpersonal@Sarahs-MacBook-Pro.local>
2025-02-09 21:06:51 +0000

    Update google sheets config env vars to use correct values

--- server/helpers/googleConfig.ts
+++ server/helpers/googleConfig.ts
@@ -2,12 +2,13 @@ import {GoogleAuth} from "google-auth-library";
 import {sheets} from '@googleapis/sheets'
 
 export const configureSheets = () => {
-    const jsonKeyFile = process.env.GOOGLE_SERVICE_ACCOUNT_KEY_FILE;
-    if(!jsonKeyFile) throw Error("Missing Service Account Key");
+    const email = process.env.GOOGLE_CLIENT_ID;
+    const key = process.env.GOOGLE_CLIENT_KEY?.replace(/\\n/g, '\n');
+    if(!email || !key) throw Error(`Missing Service Account Key`);
     const jwtClient = new GoogleAuth({
         credentials: {
-            "client_email": process.env.GOOGLE_CLIENT_ID,
-            "private_key": process.env.GOOGLE_CLIENT_KEY,
+            "client_email": email,
+            "private_key": key,
         },
         scopes: ['https://www.googleapis.com/auth/spreadsheets'],
     });
